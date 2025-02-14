1808cdbfa500d0c1f7c319d1661ed623fe2ba3ff
dimaakimm <dmitriykim2005@gmail.com>
2025-02-15 00:22:13 +0300

    init

---
+++ ExtensionExample/js/background.js
@@ -0,0 +1,19 @@
+﻿chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log("[background.js] Получено сообщение:", message);
+    if (message.command === "startRecording") {
+        console.log("[background.js] Начинаем запись...");
+        chrome.storage.local.set({ recording: true });
+    }
+    if (message.command === "stopRecording") {
+        console.log("[background.js] Останавливаем запись...");
+        chrome.storage.local.set({ recording: false });
+    }
+    if (message.audioUrl) {
+        console.log("[background.js] Скачиваем аудиофайл:", message.audioUrl);
+        chrome.downloads.download({
+            url: message.audioUrl,
+            filename: "mts_link_audio.webm",
+            saveAs: true
+        });
+    }
+});
